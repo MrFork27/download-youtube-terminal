f6fed9afee3b824eb110bba87457fe818063a650
MrFork27 <aitormeleropicon@gmail.com>
2024-01-29 13:59:43 +0100

    Feat: Descargar música
    Ya se puede descargar música introduciendo la URL de un vídeo por parámetros.

--- src/actions.py
+++ src/actions.py
@@ -1,3 +1,7 @@
+import os
+from pytube import YouTube, extract
+
+
 def print_help():
     usage_string = "usage: [-h] [url]\n\n"
     info_string = "Command line application to download youtube video audios.\n\n"
@@ -10,4 +14,18 @@ def print_help():
 
 
 def download_audio(url):
-    print(f"Downloading audio from {url}")
+    AUDIO_FORMAT = ".mp3"
+    MUSIC_DIRECTORY = "./music/"
+    try:
+        yt = YouTube(url)
+        print(f"Downloading audio from {yt.title}")
+        audio_streams = yt.streams.filter(only_audio=True).first()
+
+        audio_file = audio_streams.download(output_path=MUSIC_DIRECTORY)
+        song_name = os.path.splitext(audio_file)[0]
+        new_file = song_name + AUDIO_FORMAT
+        os.rename(audio_file, new_file)
+
+    except Exception as error:
+        print(error.__class__)
+        print(error)
