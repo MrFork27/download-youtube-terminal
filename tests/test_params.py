7941612234ae38de7b0307b38b51cc84b6ac040a
MrFork27 <aitormeleropicon@gmail.com>
2024-01-30 18:20:01 +0100

    Test: Testar función get_param_action de params
    He implementado un primer test para la función get_param_action de params.py.

--- tests/test_params.py
+++ tests/test_params.py
@@ -1,12 +1,32 @@
 import unittest
+import sys
+import src.params as params
+from unittest.mock import patch
 
 
 class TestParams(unittest.TestCase):
     def test_get_param_action(self):
-        data1 = 1
-        data2 = 2
-        result = data1 + data2
-        self.assertEqual(result, 3)
+        help_short_arguments = ["./src/main.py", "-h"]
+        help_long_arguments = ["./src/main.py", "--help"]
+        url_short_arguments = ["./src/main.py", "-u", "asdf"]
+        url_long_arguments = ["./src/main.py", "--url", "asdf"]
+        error_arguments = ["./src/main.py", "fdkla", "asdf"]
+
+        with patch.object(sys, "argv", help_short_arguments):
+            param_action = params.get_param_action()
+            self.assertEqual(param_action, params.ParamsType.HELP)
+        with patch.object(sys, "argv", help_long_arguments):
+            param_action = params.get_param_action()
+            self.assertEqual(param_action, params.ParamsType.HELP)
+        with patch.object(sys, "argv", url_short_arguments):
+            param_action = params.get_param_action()
+            self.assertEqual(param_action, params.ParamsType.URL)
+        with patch.object(sys, "argv", url_long_arguments):
+            param_action = params.get_param_action()
+            self.assertEqual(param_action, params.ParamsType.URL)
+        with patch.object(sys, "argv", error_arguments):
+            param_action = params.get_param_action()
+            self.assertEqual(param_action, params.ParamsType.ERROR)
 
 
 if __name__ == "___main___":
