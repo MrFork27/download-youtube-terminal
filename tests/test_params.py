5368dc2b86eb7f199ca569c682ffd32ba7c93a45
MrFork27 <aitormeleropicon@gmail.com>
2024-03-19 12:05:30 +0100

    test: Test download video params option

--- tests/test_params.py
+++ tests/test_params.py
@@ -8,8 +8,10 @@ class TestParams(unittest.TestCase):
     def test_get_param_action(self):
         help_short_arguments = ["./src/main.py", "-h"]
         help_long_arguments = ["./src/main.py", "--help"]
-        url_short_arguments = ["./src/main.py", "-u", "asdf"]
-        url_long_arguments = ["./src/main.py", "--url", "asdf"]
+        audio_short_arguments = ["./src/main.py", "-a", "asdf"]
+        audio_long_arguments = ["./src/main.py", "--audio", "asdf"]
+        video_short_arguments = ["./src/main.py", "-v", "asdf"]
+        video_long_arguments = ["./src/main.py", "--video", "asdf"]
         error_arguments = ["./src/main.py", "fdkla", "asdf"]
 
         with patch.object(sys, "argv", help_short_arguments):
@@ -18,23 +20,32 @@ class TestParams(unittest.TestCase):
         with patch.object(sys, "argv", help_long_arguments):
             param_action = params.get_param_action()
             self.assertEqual(param_action, params.ParamsType.HELP)
-        with patch.object(sys, "argv", url_short_arguments):
+        with patch.object(sys, "argv", audio_short_arguments):
             param_action = params.get_param_action()
-            self.assertEqual(param_action, params.ParamsType.URL)
-        with patch.object(sys, "argv", url_long_arguments):
+            self.assertEqual(param_action, params.ParamsType.AUDIO)
+        with patch.object(sys, "argv", audio_long_arguments):
             param_action = params.get_param_action()
-            self.assertEqual(param_action, params.ParamsType.URL)
+            self.assertEqual(param_action, params.ParamsType.AUDIO)
+        with patch.object(sys, "argv", video_short_arguments):
+            param_action = params.get_param_action()
+            self.assertEqual(param_action, params.ParamsType.VIDEO)
+        with patch.object(sys, "argv", video_long_arguments):
+            param_action = params.get_param_action()
+            self.assertEqual(param_action, params.ParamsType.VIDEO)
         with patch.object(sys, "argv", error_arguments):
             param_action = params.get_param_action()
             self.assertEqual(param_action, params.ParamsType.ERROR)
 
     def test_check_param(self):
         HELP_PARAMS = ["-h", "--help"]
-        URL_PARAMS = ["-u", "--url"]
+        AUDIO_PARAMS = ["-a", "--audio"]
+        VIDEO_PARAMS = ["-v", "--video"]
         help_short_arguments = ["./src/main.py", "-h"]
         help_long_arguments = ["./src/main.py", "--help"]
-        url_short_arguments = ["./src/main.py", "-u", "asdf"]
-        url_long_arguments = ["./src/main.py", "--url", "asdf"]
+        audio_short_arguments = ["./src/main.py", "-a", "asdf"]
+        audio_long_arguments = ["./src/main.py", "--audio", "asdf"]
+        video_short_arguments = ["./src/main.py", "-v", "asdf"]
+        video_long_arguments = ["./src/main.py", "--video", "asdf"]
         error_arguments = ["./src/main.py", "fdkla"]
 
         try:
@@ -44,15 +55,28 @@ class TestParams(unittest.TestCase):
                 params.check_param(HELP_PARAMS)
         except:
             self.fail(f"check_param with {HELP_PARAMS} raised Exception")
+
         try:
-            with patch.object(sys, "argv", url_short_arguments):
-                params.check_param(URL_PARAMS)
-            with patch.object(sys, "argv", url_long_arguments):
-                params.check_param(URL_PARAMS)
+            with patch.object(sys, "argv", audio_short_arguments):
+                params.check_param(AUDIO_PARAMS)
+            with patch.object(sys, "argv", audio_long_arguments):
+                params.check_param(AUDIO_PARAMS)
         except:
-            self.fail(f"check_param with {URL_PARAMS} raised Exception")
-        with patch.object(sys, "argv", error_arguments):
-            self.assertRaises(Exception, params.check_param, HELP_PARAMS)
+            self.fail(f"check_param with {AUDIO_PARAMS} raised Exception")
+
+        try:
+            with patch.object(sys, "argv", video_short_arguments):
+                params.check_param(VIDEO_PARAMS)
+            with patch.object(sys, "argv", video_long_arguments):
+                params.check_param(VIDEO_PARAMS)
+        except:
+            self.fail(f"check_param with {VIDEO_PARAMS} raised Exception")
+
+        try:
+            with patch.object(sys, "argv", error_arguments):
+                params.check_param(HELP_PARAMS)
+        except:
+            self.fail(f"check_param with {error_arguments} raised Exception")
 
 
 if __name__ == "___main___":
